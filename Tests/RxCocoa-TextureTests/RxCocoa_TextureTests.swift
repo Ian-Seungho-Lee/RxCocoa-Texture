b6592c0785a1efdf0ba971d4f91e1e8c7b07a9b0
elesahich <ian@channel.io>
2022-12-16 20:43:41 +0900

    Add Package.swift file

---
+++ Tests/RxCocoa-TextureTests/RxCocoa_TextureTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RxCocoa_Texture
+
+final class RxCocoa_TextureTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RxCocoa_Texture().text, "Hello, World!")
+    }
+}
