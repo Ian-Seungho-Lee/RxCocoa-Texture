3ab39da128ff31cf3336cc0b13ac2806a963e43f
elesahich <ian@channel.io>
2022-12-19 20:17:48 +0900

    Adding RxSwift and Texture Dependencies

--- Package.swift
+++ Package.swift
@@ -15,13 +15,20 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
+    .package(
+      url: "https://github.com/3a4oT/Texture.git", .upToNextMajor(from: "3.0.0")
+    ),
+    .package(
+      url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0")
+    )
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "RxCocoa-Texture",
-      dependencies: []
-    ),
-  ]
+      path: "RxCocoa-Texture/Classes"
+    )
+  ],
+  swiftLanguageVersions: [.v5]
 )
